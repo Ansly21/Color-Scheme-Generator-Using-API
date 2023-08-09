464c06ab0482279c970b1c69c0cbd6d93ff38f52
Ansly21 <anslybrandon.dy.iics@ust.edu.ph>
2023-08-09 17:35:16 +0800

    Added contents

---
+++ index.js
@@ -0,0 +1,60 @@
+const inputColor = document.getElementById("colorBox")
+const containerDiv = document.getElementById("container")
+const form = document.getElementById("form")
+const mode = document.getElementById("mode")
+
+inputColor.addEventListener("change", function() {
+    console.log(inputColor.value)
+})
+
+
+let data = {}
+let valuesArray = []
+
+form.addEventListener("submit", (e) => {
+    containerDiv.innerHTML = ""
+    valuesArray = []
+             
+    e.preventDefault()
+    console.log("btn clicked")
+    console.log(mode.value)
+    console.log(inputColor.value)
+
+    let inputColorValue = inputColor.value.replace("#", "")
+
+    let completeURL = {
+        color: inputColorValue,
+        mode: mode.value
+    }
+
+    renderHtml(completeURL)
+})
+
+
+
+
+function renderHtml(data) {
+   
+    fetch(`https://www.thecolorapi.com/scheme?hex=${data.color}&mode=${data.mode}&count=5`)
+    .then(res => res.json())
+    .then(data => {
+       for (color of data.colors) {
+            console.log(color.hex.value)
+            
+            valuesArray.push(color.hex.value)
+            containerDiv.innerHTML += `<div style="background:${color.hex.val}"></div>`
+         
+       }
+
+       for (let i = 0; i < valuesArray.length; i++) {
+            containerDiv.innerHTML += `<div class="colorDiv" style="background: ${valuesArray[i]}"> </div>`
+            
+        
+       }
+      
+    })
+
+} 
+
+   
+    console.log(valuesArray)

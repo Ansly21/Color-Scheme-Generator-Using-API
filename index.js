032466bd2e1c72f12df2bcd3f27f12d0a41425b3
Ansly21 <anslybrandon.dy.iics@ust.edu.ph>
2023-08-10 23:15:31 +0800

    Made it mini responsive and change hex texts font

--- index.js
+++ index.js
@@ -3,47 +3,28 @@ const containerDiv = document.getElementById("container")
 const form = document.getElementById("form")
 const mode = document.getElementById("mode")
 
-inputColor.addEventListener("change", function() {
-    console.log(inputColor.value)
-})
-
-
 let data = {}
 let valuesArray = []
 
 form.addEventListener("submit", (e) => {
-    containerDiv.innerHTML = ""
-    valuesArray = []
-             
     e.preventDefault()
-    console.log("btn clicked")
-    console.log(mode.value)
-    console.log(inputColor.value)
-
+    containerDiv.innerHTML = ""
+    valuesArray = []         
     let inputColorValue = inputColor.value.replace("#", "")
-
     let completeURL = {
         color: inputColorValue,
         mode: mode.value
     }
-
     renderHtml(completeURL)
 })
 
-
-
-
 function renderHtml(data) {
-   
     fetch(`https://www.thecolorapi.com/scheme?hex=${data.color}&mode=${data.mode}&count=5`)
     .then(res => res.json())
     .then(data => {
        for (color of data.colors) {
-            console.log(color.hex.value)
-            
             valuesArray.push(color.hex.value)
             containerDiv.innerHTML += `<div style="background:${color.hex.val}"></div>`
-         
        }
 
        for (let i = 0; i < valuesArray.length; i++) {
@@ -52,13 +33,6 @@ function renderHtml(data) {
                 <div class="hexText">${valuesArray[i]}</div>
             </div>
             `
-            
-        
        }
-      
     })
-
 } 
-
-   
-    console.log(valuesArray)
